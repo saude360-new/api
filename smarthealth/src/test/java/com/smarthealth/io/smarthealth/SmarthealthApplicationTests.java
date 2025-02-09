48975f4abe34c83168f2ce50b8412fe575a4293a
gitlab-az1 <gitlab.az1@gmail.com>
2025-02-09 11:06:34 -0300

    feat: initial setup of sql query builder

--- smarthealth/src/test/java/com/smarthealth/io/smarthealth/SmarthealthApplicationTests.java
+++ smarthealth/src/test/java/com/smarthealth/io/smarthealth/SmarthealthApplicationTests.java
@@ -3,11 +3,19 @@ package com.smarthealth.io.smarthealth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.smarthealth.io.smarthealth.shared.core.QueryBuilder;
+
+
 @SpringBootTest
 class SmarthealthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  public static void main(String[] args) throws IllegalArgumentException, Exception {
+    final QueryBuilder builder = new QueryBuilder("users", "x")
+      .select(new String[]{ "user_id", "tax_cpf", "nuked_at" });
 
+    System.out.println(builder.where("WHERE x.user_id = $1"));
+  }
+
+	@Test
+	void contextLoads() { }
 }
